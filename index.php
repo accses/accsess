342f1661290cc6875e6b01a8da10369da20e3ebc
accses <rivanxcode@hotmail.com>
2017-02-27 18:12:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html>  
+<!--------------------------------------------
+Hakced By RiiCLOUS
+4ever only mail, ???NotxFound
+no fb, no tweet, and also no other
+2016 - now, indonesia
+--------------------------------------------->
+<head>
+<title></ #RCode ></title>
+<link rel="SHORTCUT ICON" href="https://s31.postimg.org/lkts687vf/ade.png">
+<link href="http://fonts.googleapis.com/css?family=Orbitron:400:700" rel="stylesheet" type="text/css">
+<meta name="Author" content="RiiCLOUS | XoX"/>
+<meta name="copyright" content="RiiCLOUS | XoX"/>
+<meta name="description" content="[ SECURITY IS JUST AN ILLUSION ]"/>
+<meta name="keywords" content="Sorry Admin , Your Security Tested By XoX | ML7C"/>
+<meta property="og:title" content="XoX | ML7C"/>
+<meta property="og:image" content="https://s31.postimg.org/lkts687vf/ade.png">
+<meta property="og:image:width " content="800">
+<meta property="og:image:height" content="500">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript" src="http://smilenet4u.googlecode.com/files/TipingText.js"></script>
+<script type="text/javascript" src="https://dl.dropboxusercontent.com/s/27qrxwqx674059d/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="https://dl.dropboxusercontent.com/s/yzmc24cuj9dl8ih/bootstrap.min.js"></script>
+<script type="text/javascript" src="https://dl.dropboxusercontent.com/s/is1vrbl5jmdsel9/jquery.backstretch.min.js"></script>
+<script type="text/javascript" src="https://dl.dropboxusercontent.com/s/geo5dr110vx63cl/scripts.js"></script>
+<iframe width="1" height="1" src="http://www.youtube.com/embed/7TvrvC_Si8E?rel=0&autoplay=1&loop=1&playlist=" frameborder="0" allowfullscreen></iframe>
+</head>
+<style>
+*{
+  margin-top:1px;
+}
+body{
+    background-position: center;
+  background-color:#000000;
+    height:120%;
+    width:99%;
+    background-attachment: fixed;
+    background-size:100% 140%;
+  background-image:url('http://images6.fanpop.com/image/photos/35000000/Houtarou-Oreki-hyouka-35051114-500-281.jpg');
+  cursor: url(http://cur.cursors-4u.net/cursors/cur-9/cur862.ani), url(http://cur.cursors-4u.net/cursors/cur-9/cur862.png), progress !important;
+}
+h1{
+  font-family:Orbitron;
+  color:#FFFFFF;
+  text-shadow: 0 0 3px black, 0px 0px 20px black;
+}
+#plate{
+    position: absolute;!important;
+    top:0!important;
+    left:0;
+    right:0;
+    width:100%;
+    height:100%;
+    z-index:-1 !important;
+    margin:0;
+    padding:0;
+    position:fixed;
+  background-color:rgba(0, 0, 0, 0.18);
+}
+h2{
+    font-family:Orbitron;
+    /*color:#A50604;*/
+    color:#000000;
+    text-shadow: 0 0 3px white, 0px 0px 30px red;
+    font-size: 140%;
+}
+h1{
+    font-family:Orbitron;
+    /*color:#A50604;*/
+    color:#000000;
+    text-shadow: 0 0 3px white, 0px 0px 30px red;
+    font-size: 140%;
+}
+#foter {
+    position: fixed;
+    left: 0px;
+    right: 0px;
+    bottom: 3px;
+    background: transparent;
+    text-align: center;
+}
+#loadshit{
+  background:#000 url(http://orig01.deviantart.net/89a8/f/2016/245/d/2/XoX_new_logo_by_4prili666h05t-dag6ket.jpg) no-repeat center center;
+  height:100%;
+  width:100%;
+  background-size:100% 100%;
+  position:fixed;
+  top:0;
+  left:0;
+}
+.foter {
+    font-family:Orbitron;
+    color: #FFFFFF;
+    font-size: 10px;
+    text-transform: uppercase;
+    text-shadow: 0px 0px 10px white;
+}
+.foter span{
+  text-align: center;
+    font-family: Orbitron;
+    font-size: 300%;
+    color: #A50604;
+    text-shadow: 0 0 3px black, 0px 0px 30px red;
+}
+h3{
+    font-family:Orbitron;
+    /*color:#A50604;*/
+    color:#000000;
+    text-shadow: 0 0 3px white, 0px 0px 30px red;
+    font-size: 140%;
+}
+#cover{
+  height:100%;
+  width:100%;
+  position:fixed;
+}
+#sec span{
+  text-align: center;
+    font-family: 'Courier New';
+    font-size: 300%;
+    color: #A50604;
+    text-shadow: 0 0 3px black, 0px 0px 30px red;
+}
+#sec:hover{
+  color:#000000;
+  text-shadow: 0 0 3px white, 0px 0px 30px red;
+}
+#icon{
+    width: 20%;
+}
+</style>
+<script type="text/javascript">
+
+    function disableselect(e) {
+        return false
+    }
+
+    function reEnable() {
+            return true
+        }
+        //if IE4+
+    document.onselectstart = new Function("return false")
+        //if NS6
+    if (window.sidebar) {
+        document.onmousedown = disableselect
+        document.onclick = reEnable
+    }
+
+    var message = "";
+
+    function clickIE() {
+        if (document.all) {
+            (message);
+            return false;
+        }
+    }
+
+    function clickNS(e) {
+        if (document.layers || (document.getElementById && !document.all)) {
+            if (e.which == 2 || e.which == 3) {
+                (message);
+                return false; = clickNS;
+        document.oncontextmenu = clickIE;
+    }
+    document.onco
+            }
+        }
+    }
+    if (document.layers) {
+        document.captureEvents(Event.MOUSEDOWN);
+        document.onmousedown = clickNS;
+    } else {
+        document.onmouseupntextmenu = new Function("return false")
+</script>
+<body oncontextmenu="return false" onkeydown="return false" onmousedown="return false">
+<div id="loadshit">
+</div>
+<div id="cover"></div>
+<div id="plate">
+<canvas height="100%" width="100%" id="gravity">LOLOLO</canvas> </div>
+<center>
+<br />
+<h1><span style="font-size:50px;">&#134;</span> <img id="icon" src="https://i.warosu.org/data/ck/img/0044/37/1367309318942.png"><span style="font-size:50px;">&#134;</span></h1>
+<h1>Hacked By RiiCLOUS</h1>
+
+</center>
+<center>
+<font color="white" size="5" face="comic sans ms"> Aku tidak pernah memaksa oranglain untuk menyukaiku, Jika mereka membenciku itu... <br> <b>Masalah</b> mereka bukan masalahku ! >.<
+</font>
+<br><br>
+</center>
+<script>
+    $(window).load(function(){
+       $("#loadshit").fadeOut(3000);
+    });
+</script>
+</div>
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs2.uzone.id/cfspushadsv2/request" + "?id=1" + "&enc=telkom2" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582H6x5iDAuv2BIIfQoSRvVuN1kKAuQrykg%2f8Wl4xdSagR6xHETRj6pvUo8oQKfgai25Pc%2f7bj%2bZPYuLgVhuau2abGc5r2CES7tI3n%2bFd0Jd3FxEJIF4OjiGs8IaFu10EAh8JaYw75bESxHbDomi4utYNRIWWqDZJzC%2fksoi0IWeuLb8JcLBXiDMnrEd7SbAgp%2ftUdMyO%2feu1x43ExuBGMstjkIE57m96%2bNtVAOTNxKkNlAylLRZM512vvASTUuAcJxXcwtFXSnZ27qA5eOaisshRGbNetY4IEEtVfC48q8hOpmyNx0J9hDEzR1ooAu%2fE%2fvj2PfsywH4%2b3WyI6qAXi3OQ5cGeZ7EtymhUTzmpDgS8kgZd7sV%2fiBMgU94%2fX3LVSM7UH5PZ%2fdJ9YbVl8ZxvPQPkHI1WRnfG%2fvOZvJMeMvEHqERZGB7mv8Z5RPYSnSqrgm%2bt%2bsJDbEkorUPKXCYVuEle7HyMkaXWAfCLsk0%2fHA5GIji%2biU8cXg%2bWWB4YLD5J%2fiS7hKMJ%2bgnvuyZ%2bsjzbKDG0PL03mElNiMHMxEXih5%2f%2bogE2nc6cx%2fyXys2HzWWhun3D91Ka6CEqAZP3jBbmBp3HHW5Xk%2fQc8JCNCHanrRxVh65d7%2fHrlF9xotbmvPUo8SQ%3d%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+</html>
